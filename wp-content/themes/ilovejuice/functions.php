397ad1ea007e0bd39fd439468ae7cf2089d941e5
vrozkovec <vit.rozkovec@gmail.com>
2014-05-12 11:01:45 +0200

    juice style

--- wp-content/themes/ilovejuice/functions.php
+++ wp-content/themes/ilovejuice/functions.php
@@ -212,6 +212,10 @@ require 'classes/Walker_Menu.php';
 // }
 
 
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+add_action( 'woocommerce_after_single_product', 'woocommerce_output_related_products', 20);
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
---
+++ wp-content/themes/ilovejuice/woocommerce/loop/loop-start.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+?>
+<!-- <ul class="products"> -->
+<div class="clearfix"></div>
+<div class="row-fluid">
